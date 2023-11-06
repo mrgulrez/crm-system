3f3fd05b3c57e321f0f420bea67f19bed0aeb5e8
Gulrez Alam <egulrezalam@gmail.com>
2023-11-06 20:06:08 +0530

    Update ALLOWED_HOSTS in settings.py

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '.netlify.app', '.now.sh', 'localhost', '127.0.0.1']
 
 # Application definition
 
